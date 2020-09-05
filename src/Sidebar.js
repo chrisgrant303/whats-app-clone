98ceabdbebb379e42d804b620a709b6f1dd49704
Chris Grant <chrisgrant303@gmail.com>
2020-09-05 13:10:01 -0500

    Chat body initialization timestamp 1:34:00

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,6 +5,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import SidebarChat from "./SidebarChat";
 
 function Sidebar() {
     return (
@@ -28,6 +29,11 @@ function Sidebar() {
                     <SearchOutlined />
                     <input placeholder="Search or start new chat" type="text" />
                 </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     );
