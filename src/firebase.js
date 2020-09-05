dfeaece5daa467b4c791f047c0007c9a54135395
Chris Grant <chrisgrant303@gmail.com>
2020-09-05 09:13:32 -0500

    Firebase config installed

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_L2-C_rkVKxwjfSMP-kqv7jD-Xxlzc_M",
+    authDomain: "whats-app-mern-43efb.firebaseapp.com",
+    databaseURL: "https://whats-app-mern-43efb.firebaseio.com",
+    projectId: "whats-app-mern-43efb",
+    storageBucket: "whats-app-mern-43efb.appspot.com",
+    messagingSenderId: "535012916698",
+    appId: "1:535012916698:web:067368de50d404533614e3"
+};
