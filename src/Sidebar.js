4bf98e04bc5a75bddec3aca7edd6f5bbae2696cb
Chris Grant <chrisgrant303@gmail.com>
2020-09-05 11:10:57 -0500

    Sidebar Header Initialized

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Sidebar.css';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>I am a sidebar</h1>
-
+            <div className="sidebar__header">
+                <Avatar src="https://image.freepik.com/free-vector/man-avatar-profile-round-icon_24640-14046.jpg" />
+                <div className="side__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Sidebar
